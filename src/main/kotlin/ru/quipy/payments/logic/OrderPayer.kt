95bd1c665f8624a67f6d27c9bde369b6154e070f
aljbet <esivannikova3@gmail.com>
2026-03-27 16:55:08 +0300

    less bucket

--- src/main/kotlin/ru/quipy/payments/logic/OrderPayer.kt
+++ src/main/kotlin/ru/quipy/payments/logic/OrderPayer.kt
@@ -59,7 +59,7 @@ class OrderPayer {
             LeakingBucketRateLimiter(
                 200,
                 Duration.ofSeconds(1),
-                10000
+                1000
             )
     }
 
