1fc49a6e560143fd89db12710af5c44ec9ff541b
aljbet <esivannikova3@gmail.com>
2026-03-27 17:52:20 +0300

    hedge 1 and delay 1000

--- src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
+++ src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
@@ -134,7 +134,7 @@ class PaymentExternalSystemAdapterImpl(
                 attempt < maxRetries && deadline - now() > avgProcMs + retryAfter && amount > price * (attempt + 1)
 
             while (!circuitBreaker.tryAcquirePermission()) {
-                delay(10000)
+                delay(5000)
             }
             semaphore.withPermit {
                 rateLimiter.tickBlocking()
@@ -181,7 +181,7 @@ class PaymentExternalSystemAdapterImpl(
         val idempotencyKey = UUID.randomUUID().toString()
         val firstDeferred = send(uri, idempotencyKey)
         val allDeferred = mutableListOf(firstDeferred)
-        for (hedgeIndex in 1..0) {
+        for (hedgeIndex in 1..1) {
             withTimeoutOrNull(hedgeDelayMs) {
                 select {
                     allDeferred.forEach { deferred ->
